9dff0fa1a53a861f21bbe86f24dd233820cb5c9c
Jim <imojim@gmail.com>
2020-05-06 23:32:40 -0500

    Hello world

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,19 +25,22 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: `20px 0`,
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer style={{
+          color: `white`,
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `20px`,
+          textAlign: `center`
+        }}>
+          © {new Date().getFullYear()}, Astoria Legend
         </footer>
       </div>
     </>
